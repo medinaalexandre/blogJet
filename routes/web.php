91ca709a25d16a81e020ccd8fac8ec358172e707
Alexandre Medina <ammedina@inf.ufsm.br>
2019-07-02 14:32:13 -0300

    limpando o web.php

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin/', 'AdminController@index')->middleware('admin.check');
 Route::resource('posts', 'PostsController')->middleware('admin.check');
 
-// links q tme q ser ajax
 Route::post('/comments/create', 'PostsController@storeComment')->name('sendComment');
 Route::post('likePost', 'LikeController@store');
 Route::post('likeComment', 'LikeController@storeLikeComment');
-
 
 Route::resource('users', 'UsersController')->middleware('admin.check');
 Route::resource('categories', 'CategoriesController')->middleware('admin.check');
@@ -32,6 +30,6 @@ Route::get('post/{slug}', 'PostsController@singlePost');
 Route::post('search', 'PostsController@search');
 Route::get('user/{user}', 'UsersController@singleUser');
 
-// login c google
+// google login
 Route::get('auth/{provider}', 'Auth\LoginController@redirectToProvider');
 Route::get('auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
